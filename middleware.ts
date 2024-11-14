1d039e5c7d280abc6e2ec7c8f067942fff90c904
ibrah11 <kingiboy94@gmail.com>
2024-11-15 00:31:23 +0300

    first commit

--- middleware.ts
+++ middleware.ts
@@ -9,18 +9,19 @@ const isProtectedRoute = createRouteMatcher([
 const isAdminRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware((auth, req: NextRequest) => {
-
     const { userId, redirectToSignIn, sessionClaims } = auth()
 
+    // Redirect to sign-in if the user is not authenticated and trying to access protected or admin routes
     if (!userId && (isProtectedRoute(req) || isAdminRoute(req))) {
         return redirectToSignIn({ returnBackUrl: req.url })
     }
 
+    // Check if the user is an admin
     if (userId && isAdminRoute(req)) {
         if (sessionClaims?.metadata.role === 'admin') {
             return NextResponse.next()
         } else {
-            return new Response(`You are not authorized`, {status: 401})
+            return new Response(`You are not authorized`, { status: 403 }) // Changed status to 403 for forbidden access
         }
     }
 
